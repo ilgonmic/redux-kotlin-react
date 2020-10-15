ac3bbe5403883dc478e21c9b46a50f7a9616a6c0
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-10-15 14:08:25 +0300

    Non exportable VisibilityFilters [use const]

--- src/main/kotlin/VisibilityFilters.kt
+++ src/main/kotlin/VisibilityFilters.kt
@@ -1,17 +1,8 @@
-object VisibilityFilters {
-    const val SHOW_ALL = "SHOW_ALL"
-    const val SHOW_ACTIVE = "SHOW_ACTIVE"
-    const val SHOW_COMPLETED = "SHOW_COMPLETED"
-}
+@JsExport
+const val SHOW_ALL = "SHOW_ALL"
 
 @JsExport
-val SHOW_ALL
-    get() = VisibilityFilters.SHOW_ALL
+const val SHOW_ACTIVE = "SHOW_ACTIVE"
 
 @JsExport
-val SHOW_ACTIVE
-    get() = VisibilityFilters.SHOW_ACTIVE
-
-@JsExport
-val SHOW_COMPLETED
-    get() = VisibilityFilters.SHOW_COMPLETED
+const val SHOW_COMPLETED = "SHOW_COMPLETED"
