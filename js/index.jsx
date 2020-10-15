beb5b8a50cc607f807c7e27089ca31a0c60bc902
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-10-15 13:16:18 +0300

    Uncomment store

--- js/index.jsx
+++ js/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from "./App"
-import {store} from "./components"
+import {store} from "kotlin-library"
+
+console.log(store.state) // undefined, why?
 
 ReactDOM.render(
     <App store={store}/>,
--- src/main/kotlin/Store.kt
+++ src/main/kotlin/Store.kt
@@ -2,30 +2,30 @@ import org.reduxkotlin.*
 
 @JsExport
 val store = createStore(::rootReducer, State())
-//    .let { origin ->
-//        object : Store<State> {
-//            override val getState: GetState<State>
-//                get() = origin.getState
-//            override var dispatch: Dispatcher
-//                get() = origin.dispatch
-//                set(value) {
-//                    origin.dispatch = value
-//                }
-//            override val subscribe: (StoreSubscriber) -> StoreSubscription
-//                get() = origin.subscribe
-//            override val replaceReducer: (Reducer<State>) -> Unit
-//                get() = origin.replaceReducer
-//
-//        }
-//    }
-//
-//// It is necessary for reexport Store type
-//@JsExport
-//interface Store<State> {
-//    val getState: GetState<State>
-//    var dispatch: Dispatcher
-//    val subscribe: (StoreSubscriber) -> StoreSubscription
-//    val replaceReducer: (Reducer<State>) -> Unit
-//    val state: State
-//        get() = getState()
-//}
+    .let { origin ->
+        object : Store<State> {
+            override val getState: GetState<State>
+                get() = origin.getState
+            override var dispatch: Dispatcher
+                get() = origin.dispatch
+                set(value) {
+                    origin.dispatch = value
+                }
+            override val subscribe: (StoreSubscriber) -> StoreSubscription
+                get() = origin.subscribe
+            override val replaceReducer: (Reducer<State>) -> Unit
+                get() = origin.replaceReducer
+
+        }
+    }
+
+// It is necessary for reexport Store type
+@JsExport
+interface Store<State> {
+    val getState: GetState<State>
+    var dispatch: Dispatcher
+    val subscribe: (StoreSubscriber) -> StoreSubscription
+    val replaceReducer: (Reducer<State>) -> Unit
+    val state: State
+        get() = getState()
+}
